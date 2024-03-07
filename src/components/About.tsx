77273e8c24a140f7f8b5db4167e5cb35040d5328
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-03-07 22:06:43 +0900

    Feat : 모션 추가

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,7 +49,7 @@ const About = () => {
             </motion.div>
 
             <motion.p
-                variants={fadeIn("", "", 0.1, 1)}
+                variants={fadeIn("bottom", "", 0.1, 1)}
                 className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
             >
                 I'm a skilled software developer with experience in TypeScript
