f30a51b647e0d160f399e26a1ca53db0cb73134f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-06 16:52:00 +0900

    feat : 블로그 아이콘 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,10 @@ const Navbar = () => {
     const handleGithub = () => {
         window.open("https://github.com/HA-SEUNG-JEONG", "_blank");
     };
+    const handleBlog = () => {
+        window.open("https://morethan-haseung-log.vercel.app/", "_blank");
+    };
+
     return (
         <nav
             className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -18,7 +22,7 @@ const Navbar = () => {
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link
                     to="/"
-                    className="flex items-center gap-1/2"
+                    className="flex items-center gap-2"
                     onClick={() => {
                         setIsActive("");
                         window.scrollTo(0, 0);
@@ -26,18 +30,30 @@ const Navbar = () => {
                 >
                     <button
                         onClick={handleGithub}
-                        className="bg-gray-500 text-white px-2 py-2 rounded-md ml-4 hover:bg-gray-600 flex items-center gap-2"
+                        className="bg-transparent text-white px-2 py-2 rounded-md ml-4 hover:opacity-80 flex items-center gap-2"
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
+                            width="32"
+                            height="32"
                             fill="currentColor"
                             viewBox="0 0 16 16"
                         >
                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                         </svg>
-                        View on Github
+                    </button>
+                    <button onClick={handleBlog}>
+                        <svg
+                            stroke="currentColor"
+                            fill="currentColor"
+                            stroke-width="0"
+                            viewBox="0 0 448 512"
+                            height="30"
+                            width="30"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M446.6 222.7c-1.8-8-6.8-15.4-12.5-18.5-1.8-1-13-2.2-25-2.7-20.1-.9-22.3-1.3-28.7-5-10.1-5.9-12.8-12.3-12.9-29.5-.1-33-13.8-63.7-40.9-91.3-19.3-19.7-40.9-33-65.5-40.5-5.9-1.8-19.1-2.4-63.3-2.9-69.4-.8-84.8.6-108.4 10C45.9 59.5 14.7 96.1 3.3 142.9 1.2 151.7.7 165.8.2 246.8c-.6 101.5.1 116.4 6.4 136.5 15.6 49.6 59.9 86.3 104.4 94.3 14.8 2.7 197.3 3.3 216 .8 32.5-4.4 58-17.5 81.9-41.9 17.3-17.7 28.1-36.8 35.2-62.1 4.9-17.6 4.5-142.8 2.5-151.7zm-322.1-63.6c7.8-7.9 10-8.2 58.8-8.2 43.9 0 45.4.1 51.8 3.4 9.3 4.7 13.4 11.3 13.4 21.9 0 9.5-3.8 16.2-12.3 21.6-4.6 2.9-7.3 3.1-50.3 3.3-26.5.2-47.7-.4-50.8-1.2-16.6-4.7-22.8-28.5-10.6-40.8zm191.8 199.8l-14.9 2.4-77.5.9c-68.1.8-87.3-.4-90.9-2-7.1-3.1-13.8-11.7-14.9-19.4-1.1-7.3 2.6-17.3 8.2-22.4 7.1-6.4 10.2-6.6 97.3-6.7 89.6-.1 89.1-.1 97.6 7.8 12.1 11.3 9.5 31.2-4.9 39.4z"></path>
+                        </svg>
                     </button>
                 </Link>
                 <ul className="list-none hidden sm:flex flex-row gap-10">
